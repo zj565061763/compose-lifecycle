956f467241a588117136fcb4d67f3fcb44e2f38f
zjun <565061763@qq.com>
2024-11-07 00:58:12 +0800

    sample

--- app/src/main/java/com/sd/demo/compose/lifecycle/App.kt
+++ app/src/main/java/com/sd/demo/compose/lifecycle/App.kt
@@ -1,9 +1,7 @@
 package com.sd.demo.compose.lifecycle
 
 import android.app.Application
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.eventFlow
-import androidx.lifecycle.repeatOnLifecycle
 import com.sd.lib.compose.lifecycle.fAppLifecycle
 import com.sd.lib.compose.lifecycle.fAppLifecycleScope
 import kotlinx.coroutines.launch
@@ -11,12 +9,6 @@ import kotlinx.coroutines.launch
 class App : Application() {
    override fun onCreate() {
       super.onCreate()
-      fAppLifecycleScope.launch {
-         fAppLifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-            logMsg { "AppLifecycle repeatOnLifecycle" }
-         }
-      }
-
       fAppLifecycleScope.launch {
          fAppLifecycle.eventFlow.collect { event ->
             logMsg { "AppLifecycle event:$event" }
